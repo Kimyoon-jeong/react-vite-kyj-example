11e9706c6e062d91ae6536bc3477c3222b367196
WIN-1G93LUSSCH4\ICT02_18 <asdf@gmail.com>
2024-10-02 11:39:55 +0900

    부트스트랩 적용 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
+import BootStrapButton from './BootstrapExample';
+import Header from './components/Header';
 
 function App() {
-  
-  const hello = "안녕하세요";
+    const hello = '안녕하세요'
 
-  const style ={
-    fontSize : 30
-  }
+    const style = {
+        fontSize: 30,
+    }
 
-  return (
-    <>
-      <div style={style}>{hello}</div>
-      <div>{hello}</div>
-    </>
-  )
+    return (
+        <>
+            <Header></Header>
+            <div style={style}>{hello}</div>
+            <div>{hello}</div>
+            <BootStrapButton />
+        </>
+    )
 }
 
 export default App
