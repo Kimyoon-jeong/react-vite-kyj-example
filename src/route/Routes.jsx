00f1418538060517809d123e726a3865b74e5894
WIN-1G93LUSSCH4\ICT02_18 <asdf@gmail.com>
2024-10-07 10:51:25 +0900

    input완성

--- src/route/Routes.jsx
+++ src/route/Routes.jsx
@@ -5,6 +5,8 @@ import ProfilePage from '../pages/ProfilePage';
 import PropsPage from '../pages/PropsPage';
 import ThreePage from './../pages/ThreePage';
 import CounterPage from '../pages/CounterPage';
+import InputPage from './../pages/InputPage';
+
 //react routes, route 사용시 배열로 관리하기
 const routes = [
     {
@@ -39,6 +41,11 @@ const routes = [
         path: '/usestate',
         element: <CounterPage />,
         title: 'useState 연습',
+    },
+    {
+        path: '/input',
+        element: <InputPage />,
+        title: 'input 연습',
     },
 ];
 
